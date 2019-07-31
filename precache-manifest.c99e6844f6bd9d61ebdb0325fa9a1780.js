d878332d7cd1c9420fe9bd69ea6757b493b02ee5
Ryley Chapman <Ryley20@yahoo.com>
2019-07-31 09:57:49 -0500

    Updates

---
+++ precache-manifest.c99e6844f6bd9d61ebdb0325fa9a1780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4997a655eee793c81aa35a8d0bfe3d5",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "995d1af2ffeeb26dab07",
+    "url": "/ClickyGame/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "437b77a9c3965853f004",
+    "url": "/ClickyGame/static/css/main.a4fe1942.chunk.css"
+  },
+  {
+    "revision": "995d1af2ffeeb26dab07",
+    "url": "/ClickyGame/static/js/2.8427f788.chunk.js"
+  },
+  {
+    "revision": "437b77a9c3965853f004",
+    "url": "/ClickyGame/static/js/main.2dfffdfb.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
